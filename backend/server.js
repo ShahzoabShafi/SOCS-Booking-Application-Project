deb5b9a1d2959aebec8c55fb5a452a4bd75de61b
shahzoabshafi03 <shahzoabshafi03@gmail.com>
2026-04-06 14:51:01 +0000

    Dashboard API created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
+const dashboardRoutes = require('./routes/dashboard');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 // Base route to test server is running
 app.get('/', (req, res) => {
