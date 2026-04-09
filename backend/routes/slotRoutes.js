cde456df13e4ca3217116b6940900fe9f710de58
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-09 15:27:12 -0400

    Fixed other JWT route stuff.

--- backend/routes/slotRoutes.js
+++ backend/routes/slotRoutes.js
@@ -5,9 +5,9 @@ const {create_slot, activate_slot, delete_slot, active_slots} = require('../cont
 const { protect } = require("../middleware/authMiddleware");
 
 // forward request to the correct controller function
-router.post("/create", create_slot);
-router.put("/:id/activate", activate_slot);
-router.delete("/:id/delete", delete_slot);
+router.post("/create", protect, create_slot);
+router.put("/:id/activate", protect, activate_slot);
+router.delete("/:id/delete", protect, delete_slot);
 router.get("/", active_slots);
 
 // export it so that server.js can use it.
