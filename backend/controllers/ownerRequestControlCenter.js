f08e17fc9fa50aa5b25efab9ed1e39a99a937121
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-11 15:26:05 -0400

    Edited what it returns such that name and email of owner is given.

--- backend/controllers/ownerRequestControlCenter.js
+++ backend/controllers/ownerRequestControlCenter.js
@@ -14,7 +14,7 @@ const get_requests = async (req, res) =>{
     // 2. validation - may not be needed since there's no actual input. 
     // is the user truly an owner?
     const owner = await db.get(
-        "SELECT id, role FROM users WHERE id = ?",
+        "SELECT user_id, role FROM users WHERE user_id = ?",
         [owner_id]);
 
     if (!owner){
@@ -27,7 +27,10 @@ const get_requests = async (req, res) =>{
 
     // 3. Obtain the meeting requests associated to this owner thru the db
     const meeting_requests = await db.all(
-        `SELECT * FROM meeting_requests WHERE owner_id = ? AND request_status = 'pending' ORDER BY created_at ASC;`,
+        `SELECT mr.start_time, mr.end_time, mr.title, mr.message, mr.request_status, mr.created_at AS request_created_at, u.name, u.email
+        FROM meeting_requests mr
+        JOIN users u ON mr.owner_id = u.user_id
+        WHERE mr.owner_id = ? AND mr.request_status = 'pending' ORDER BY request_created_at ASC;`, // getting some useful info from users too.
         [owner_id]
     );
 
