bfcfd1bb6b9a999939c97fe4e7b85dd2fef6ddbd
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-11 15:29:03 -0400

    modified it to return the REQUESTER's info to the owner instead.

--- backend/controllers/ownerRequestControlCenter.js
+++ backend/controllers/ownerRequestControlCenter.js
@@ -29,7 +29,7 @@ const get_requests = async (req, res) =>{
     const meeting_requests = await db.all(
         `SELECT mr.start_time, mr.end_time, mr.title, mr.message, mr.request_status, mr.created_at AS request_created_at, u.name, u.email
         FROM meeting_requests mr
-        JOIN users u ON mr.owner_id = u.user_id
+        JOIN users u ON mr.user_id = u.user_id
         WHERE mr.owner_id = ? AND mr.request_status = 'pending' ORDER BY request_created_at ASC;`, // getting some useful info from users too.
         [owner_id]
     );
