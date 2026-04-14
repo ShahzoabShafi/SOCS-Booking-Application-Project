c036bd89c388de1492e0d7386c818e935f062ad3
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-14 14:34:10 -0400

    Info about meeting participants is now given

--- backend/controllers/confirmedBookingsController.js
+++ backend/controllers/confirmedBookingsController.js
@@ -2,7 +2,7 @@
 const dbPromise = require('../config/db');
 
 // link for this function will be (get) /api/confirmedBookings
-// this function allows any user to view all booking requests aimed at them.
+// this function allows any user to view all confirmed booking requests aimed at them.
 const confirmed_bookings = async (req, res) =>{
 
     // new db config requires this approach
@@ -15,19 +15,32 @@ const confirmed_bookings = async (req, res) =>{
     // 2. validation - not needed since there's no actual input. 
 
     // 3. Obtain the confirmed bookings associated to this owner thru the db
-    const bookings = await db.all(
-        `SELECT b.created_at AS booking_created_at, s.slot_title, s.start_time, s.end_time, s.slot_type
+    // get confirmed bookings associated to this owner, and also retrieve details about the person they're meeting with.
+    // the latter part can only be done slightly convolutedly - we look for the other (should be only one) booking that
+    // has the same slot_id, and use that slot_id as a bridge through slots to ther users table.
+    const confirmed_bookings_info = await db.all(
+        `
+        SELECT 
+
+        b.created_at AS booking_created_at, b.booking_id,
+        s.slot_title, s.start_time, s.end_time, s.slot_type,
+        u.email AS meeting_partner_email, u.name AS meeting_partner_name
+
         FROM bookings b
         JOIN slots s ON b.slot_id = s.slot_id
-        WHERE b.user_id = ? 
-        ORDER BY booking_created_at ASC;`,
-        [user_id]
+        JOIN bookings b2 ON b2.slot_id = b.slot_id AND b2.user_id != ?
+        JOIN users u ON b2.user_id = u.user_id
+        WHERE b.user_id = ?
+
+        ORDER BY booking_created_at ASC;
+        `, 
+        [user_id, user_id]
     );
 
     // 4. return crap
     // note returning an empty array is ok / thats what we want. (?) (talk to frontend)
     return res.status(200).json({ message: "Bookings retrieved successfully!",
-                                  all_confirmed_bookings: bookings });
+                                  all_confirmed_bookings: confirmed_bookings_info});
 };
 
 module.exports = {confirmed_bookings};
