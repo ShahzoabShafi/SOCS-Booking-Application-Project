de2951f5b7966147ec6b48c00fe8e7f402294b50
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-15 15:00:14 -0400

    small change.

--- backend/controllers/confirmedBookingsController.js
+++ backend/controllers/confirmedBookingsController.js
@@ -189,4 +189,7 @@ const confirmed_bookings = async (req, res) =>{
 //
 //
 //
-//module.exports = {confirmed_bookings, delete_booking};
+
+
+
+module.exports = {confirmed_bookings, delete_booking};
