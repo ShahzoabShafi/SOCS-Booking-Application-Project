ae33d2e7139da9992354dc3da72681a1220bb04f
marielefevre2035 <marielefevre2035@gmail.com>
2026-04-16 18:55:47 +0000

    undid everything for the tabs library

--- frontend/src/pages/Slots.jsx
+++ frontend/src/pages/Slots.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import '../Dashboard.css';
-import * as Tabs from "@radix-ui/react-tabs";
 //Marie Lefevre
 
 function Slots() {
@@ -99,28 +98,7 @@ function Slots() {
                 </div>
             </div>
             <h1> My slots </h1>
-            <Tabs.Root className="TabsRoot" defaultValue="inactive">
-                <Tabs.List className="TabsList" aria-label="Manage your Slots">
-                    <Tabs.Trigger className="TabsTrigger" value="inactive">
-                        Inactive Slots
-                    </Tabs.Trigger>
-                    <Tabs.Trigger className="TabsTrigger" value="group">
-                        Open Group Meetings
-                    </Tabs.Trigger>
-                    <Tabs.Trigger className="TabsTrigger" value="oh">
-                        Office Hours
-                    </Tabs.Trigger>
-		        </Tabs.List>
-                <Tabs.Content className="TabsContent" value="inactive">
-
-                </Tabs.Content>
-                <Tabs.Content className="TabsContent" value="group">
-
-                </Tabs.Content>
-                <Tabs.Content className="TabsContent" value="oh">
-
-                </Tabs.Content>
-            </Tabs.Root>
+            
             <div id="slotList">
                 {slots.map((slot, index) => (
                     <div className="card" key={index}>
