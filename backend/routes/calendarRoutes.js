dd84210f00504c752652e93b3ada9e628f789563
Ryan Hull <ryan.h.hull@gmail.com>
2026-04-17 15:30:56 -0400

    Changed post to get for calendar

--- backend/routes/calendarRoutes.js
+++ backend/routes/calendarRoutes.js
@@ -5,7 +5,8 @@ const {export_calendar} = require('../controllers/calendarControllers');
 const { protect } = require("../middleware/authMiddleware");
 
 // forward request to the correct controller functions
-router.post("/export", protect, export_calendar);
+router.get("/export", protect, export_calendar);
+
 
 // export it so that server.js can use it.
 module.exports = router;
